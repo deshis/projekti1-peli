a016d14396f5a19f524919f04ad74a924214f6d5
Altti Äyräs <asayra@utu.fi>
2024-01-14 17:47:24 +0200

    Aiming and crosshair

--- player.gd
+++ player.gd
@@ -14,17 +14,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	#movement
 	var velocity = Vector2.ZERO
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
 	if velocity.length() > 0:
 		if velocity.length()>1:
 			velocity=velocity.normalized() #stop character from moving faster diagonally
 		velocity = velocity * speed
-	
 	position += velocity * delta #change character position
 	position = position.clamp(Vector2.ZERO, screen_size) #prevent character from moving off screen
-
 
 func _on_body_entered(body):
 
