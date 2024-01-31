b3fbbf98cc33c6b14d231aa2bd7be846729910b4
Altti Äyräs <asayra@utu.fi>
2024-01-31 14:28:12 +0200

    pause menu

--- inventory.gd
+++ inventory.gd
@@ -12,17 +12,27 @@ var item
 var selectedModeContainer
 
 var swapping
+var paused
 var selectedIndex
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	swapping = false
+	paused = false
 	selectedIndex = 0
-	if(!mode):
+	if(!mode): #pause menu
+		paused = true
 		get_child(0).get_child(2).set_process(false)
 		get_child(0).get_child(2).visible=false
-	else:
+		
+		get_child(0).get_child(3).set_process(true)
+		get_child(0).get_child(3).visible=true
+		
+		_highlightPauseMenu(0)
+	else: #pick up new mode
 		get_child(0).get_child(2).set_process(true)
+		get_child(0).get_child(3).set_process(false)
+		get_child(0).get_child(3).visible=false
 		textureBox.texture=load("res://sprites/"+mode[0]+".png")
 		textBox.push_font_size(24)
 		textBox.append_text("[center]")
@@ -43,7 +53,7 @@ func _set_swapping(s):
 
 
 func _process(_delta):
-	if(swapping):
+	if(swapping): #selecting a new mode 
 		if(Input.is_action_just_pressed("UI_up")):
 			if(selectedIndex<=0):
 				selectedIndex=4
@@ -61,10 +71,38 @@ func _process(_delta):
 			get_parent()._close_inventory()
 			get_parent().get_child(3).get_child(0)._update_firing_mode_ui()
 			item.queue_free()
-		
+	elif(paused): #pause menu
+		if(Input.is_action_just_pressed("UI_up")):
+			if(selectedIndex<=0):
+				selectedIndex=1
+			else:
+				selectedIndex-=1
+			_highlightPauseMenu(selectedIndex)
+		if(Input.is_action_just_pressed("UI_down")):
+			if(selectedIndex>=1):
+				selectedIndex=0
+			else:
+				selectedIndex+=1
+			_highlightPauseMenu(selectedIndex)
+		if(Input.is_action_just_pressed("accept")):
+			if(selectedIndex==0): #close the menu and continue game
+				get_parent()._close_inventory() 
+			elif(selectedIndex==1): #quit the game
+				get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+				get_tree().quit()
+
+
 func _highlight(index):
 	for i in currentModes.get_children().size():
 		if(i==index):
 			currentModes.get_child(i).get_child(1).set_visible(true)
 		else:
 			currentModes.get_child(i).get_child(1).set_visible(false)
+
+
+func _highlightPauseMenu(index):
+	for i in range(0,get_child(0).get_child(3).get_children().size()):
+		if(i==index):
+			get_child(0).get_child(3).get_child(i).get_child(2).set_visible(true)
+		else:
+			get_child(0).get_child(3).get_child(i).get_child(2).set_visible(false)
