b6466c1b1a21d039576982adf74c00b80351d24f
Altti Äyräs <altti.ayras@gmail.com>
2024-02-06 21:22:50 +0200

    game director difficulty level

--- GameDirector.gd
+++ GameDirector.gd
@@ -11,11 +11,8 @@ extends Node2D
 var meleeEnemy = preload("res://MeleeEnemy.tscn")
 var rangedEnemy = preload("res://RangedEnemy.tscn")
 
-
-var phase = 1
+var difficultyLevel = 1
 var credits = 0.0
-
-var spawnPositions
 
 var rng = RandomNumberGenerator.new()
 
@@ -25,18 +22,18 @@ func _ready():
 	spawnTimer.start(rng.randf_range(minSpawnTime, maxSpawnTime))
 
 
-func _process(_delta):
-	print (Engine.get_frames_per_second())
+#The difficulty level increases the director's budget, amount of enemies spawned and the bias of randomly generated modes
+#The game will get harder with time but the player also gets more powerful.
+func _on_difficulty_timer_timeout():
+	difficultyLevel+=1
+
 
 func _get_bias():
-	if(phase==1):
-		return 0
-	return 0
+	return difficultyLevel/20.0
 
 
 func _on_money_timer_timeout():
-	if(phase==1):
-		credits+=1
+	credits+=difficultyLevel/2.0
 
 
 func _on_spawn_timer_timeout():
@@ -45,12 +42,10 @@ func _on_spawn_timer_timeout():
 	var spawnLocation
 	var validSpawnLocationFound = false
 	
-	
 	#try to find a valid spawn location off screen up to 3 times. if a valid location is not found, spawn on top of player
 	for i in 3:
 		spawnLocation = global_position+Vector2(rng.randf_range(-1,1), rng.randf_range(-1,1)).normalized()*screen_size.x/2
 		var spawnTile = tileMap.get_cell_tile_data(0, tileMap.local_to_map(spawnLocation))
-		print("trying to spawn at "+str(spawnTile))
 		if(spawnTile):
 			if(spawnTile.get_collision_polygons_count(0)==0):
 				validSpawnLocationFound=true
@@ -59,12 +54,11 @@ func _on_spawn_timer_timeout():
 	if(!validSpawnLocationFound):
 		spawnLocation=global_position
 	
-	
 	#the director uses its credits every time a spawn opportunity happens
 	#the amount of enemies is randomized and the credits are split evenly among the spawned enemies
 	#for example, the director could spawn a few strong enemies or many weaker enemies
-	if(phase==1):
-		enemyAmount = rng.randi_range(1, 5)
+	@warning_ignore("integer_division")
+	enemyAmount = rng.randi_range(1, 3+difficultyLevel/2)
 	var budget = credits/enemyAmount
 	while(credits>=1):
 		if(rng.randi_range(0,1)==1): #spawn melee enemy
