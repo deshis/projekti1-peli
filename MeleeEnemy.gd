2a2b19e13bf2f10eac9218272f1123b478f80db6
Altti Äyräs <altti.ayras@gmail.com>
2024-02-11 15:29:57 +0200

    melee enemy animation and sprites

--- MeleeEnemy.gd
+++ MeleeEnemy.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 @onready var healthBar = get_node("ProgressBar")
 
 var cooldownActive = false
-@export var threshold = 50
+@export var threshold = 30
 @export var cooldown = 1.5
 
 @export var damage = 10
@@ -24,6 +24,12 @@ var item = preload("res://item_pick_up.tscn")
 
 var budget
 
+var animationThreshold = 0
+@onready var animatedSprite = get_node("AnimatedSprite2D")
+var animDirection
+var animationPlaying = false
+
+
 func _ready():
 	if(budget):
 		SPEED=SPEED+SPEED*budget/100
@@ -34,18 +40,57 @@ func _ready():
 	healthBar.value = currentHealth
 	_update_target()
 
+
 func _physics_process(_delta):
 	if(global_position.distance_to(target.global_position)>=threshold&&!cooldownActive):
 		var direction = to_local(navAgent.get_next_path_position()).normalized()
 		navAgent.set_velocity(direction*SPEED)
 	else: 
 		navAgent.set_velocity(Vector2(0,0))
+	
+	
+	animDirection = velocity.normalized()
+	if(!animationPlaying):
+		if(animDirection.x>animationThreshold):
+			if(animDirection.y<-animationThreshold):
+				animatedSprite.play("move_up_right")
+				animatedSprite.set_flip_h(false)
+			elif(animDirection.y>animationThreshold):
+				animatedSprite.play("move_down_left")
+				animatedSprite.set_flip_h(true)
+		elif(animDirection.x<-animationThreshold):
+			if(animDirection.y<-animationThreshold):
+				animatedSprite.play("move_up_right")
+				animatedSprite.set_flip_h(true)
+			elif(animDirection.y>animationThreshold):
+				animatedSprite.play("move_down_left")
+				animatedSprite.set_flip_h(false)
+
+
+func play_attack_animation(dir):
+	if(dir.x>animationThreshold):
+		if(dir.y<-animationThreshold):
+			animatedSprite.play("attack_up_right")
+			animatedSprite.set_flip_h(false)
+		elif(dir.y>animationThreshold):
+			animatedSprite.play("attack_down_left")
+			animatedSprite.set_flip_h(true)
+	elif(dir.x<-animationThreshold):
+		if(dir.y<-animationThreshold):
+			animatedSprite.play("attack_up_right")
+			animatedSprite.set_flip_h(true)
+		elif(dir.y>animationThreshold):
+			animatedSprite.play("attack_down_left")
+			animatedSprite.set_flip_h(false)
+	animationPlaying=true
+
 
 func _on_timer_timeout(): #refresh target
 	if(global_position.distance_to(target.global_position)<=threshold&&!cooldownActive):
 		_attack()
 	else:
 		_update_target()
+
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity):
 	velocity = safe_velocity
@@ -56,22 +101,20 @@ func _attack():
 	cooldownActive = true 
 	cooldownTimer.start(rng.randf_range(0.95, 1.05)*cooldown) #variation to prevent all enemies from checking on same frame
 	var aimDirection = target.global_position-global_position
-	#aimDirection=aimDirection.normalized()
 	var instance = meleeHitbox.instantiate()
 	instance.position=global_position+aimDirection/1.2
 	instance.set_damage(damage)
-	instance.get_node("AnimatedSprite2D").set_scale(Vector2(3, 3))
 	instance.get_node("CollisionShape2D").set_scale(Vector2(3, 3))
 	instance.look_at(target.global_position)
-	await get_tree().create_timer(0.15).timeout #pause to give player time to dodge
-	instance.get_node("AnimatedSprite2D").set_scale(Vector2(1, 1))
+	await get_tree().create_timer(0.1).timeout #pause to give player time to dodge
 	instance.get_node("CollisionShape2D").set_scale(Vector2(1, 1))
 	get_tree().current_scene.add_child(instance)
-	
+	play_attack_animation(aimDirection.normalized())
 
 
 func _on_cooldown_timer_timeout():
 	cooldownActive = false
+	animationPlaying=false
 
 func _update_target(): 
 	await Engine.get_main_loop().process_frame
